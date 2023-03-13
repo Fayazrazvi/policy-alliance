f4dc0be6011c4a1d26185e534913f588783d21b0
Fayazrazvi <razvifayaz12@gmail.com>
2023-03-13 18:19:27 +0530

    I'have created two more frontend pages for my miniproject

--- project/src/main/java/com/perfect/project/repository/UserRepository.java
+++ project/src/main/java/com/perfect/project/repository/UserRepository.java
@@ -5,18 +5,18 @@ import java.util.Map;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
-import com.perfect.project.entity.Company;
 import com.perfect.project.entity.Payment;
-import com.perfect.project.entity.User;
 
 
-
+@Repository
+@Component
 public interface UserRepository extends JpaRepository<Payment , Integer> {
 	
-//	@Query(nativeQuery=true,value="Select * from user_details")
-//	public List<Map<String ,Object>> out ();
+	@Query(nativeQuery=true,value="Select * from user_details where mobile_number='7010105285'")
+	public List<Map<String ,Object>> display();
 	
 
 }
