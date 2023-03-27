98d7279575455d00eb107276b29ec2ea7cec3150
Fayazrazvi <razvifayaz12@gmail.com>
2023-03-27 17:21:03 +0530

    I've done almost myproject except insertion of data

--- project/src/main/java/com/perfect/project/controller/UserController.java
+++ project/src/main/java/com/perfect/project/controller/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -19,6 +20,8 @@ import com.google.gson.Gson;
 import com.perfect.project.entity.User;
 import com.perfect.project.repository.UserRepository;
 import com.perfect.project.service.impl.UserServiceImpl;
+
+import ch.qos.logback.core.model.Model;
 
 @Controller
 //@RestController
@@ -48,6 +51,7 @@ public class UserController {
 	public String listUsers() {
 		return "users";
 	}
+	
 	
 	@GetMapping("/register")
 	public String register() {
--- project/src/main/java/com/perfect/project/entity/Company.java
+++ project/src/main/java/com/perfect/project/entity/Company.java
@@ -16,13 +16,14 @@ import java.util.Random;
 @Entity
 @Table(name = "company_details")
 public class Company {
-	@Column(name = "user_id")
-    private int user_id;
 	
 	
 	@Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private int company_id;
+	
+	@Column(name = "star_rating")
+    private int star_rating;
 	
 	
 	@Column(name = "company_name")
@@ -33,19 +34,23 @@ public class Company {
 	private int premium_amount;
 	
 	
-	@Column(name="company_iamge")
+	@Column(name="company_image")
 	private String company_image;
+	
+	@Column(name="plan")
+	private String plan;
 	
 	
 	public Company() {
 
 	    }
-	 public Company(int user_id , int company_id ,String company_name , int premium_amount, String company_image) {
-		 this.user_id=user_id;
+	 public Company(int star_rating , int company_id ,String company_name , int premium_amount, String company_image ,  String plan) {
+		 this.star_rating=star_rating;
 		 this.company_id=company_id;
 		 this.company_name=company_name;
 		 this.premium_amount=premium_amount;
 		 this.company_image=company_image;
+		 this.plan=plan;
 	 }
 	 
 
@@ -57,15 +62,21 @@ public class Company {
 	        this.company_id = company_id;
 	    }
 	    
-	    public int getuser_id() {
-	        return user_id;
+	    public int getstar_rating() {
+	        return star_rating;
 	    }
 
-	    public void setCust_id(int user_id) {
-	        this.user_id = user_id;
+	    public void setCust_id(int star_rating) {
+	        this.star_rating = star_rating;
 	    }
 	    
-	    public String getcompany_name() {
+	    public String getPlan() {
+			return plan;
+		}
+		public void setPlan(String plan) {
+			this.plan = plan;
+		}
+		public String getcompany_name() {
 	        return company_name;
 	    }
 
